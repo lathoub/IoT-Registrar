31cce58d566f213173dfe07d349bd3d1ad83c6c2
lathoub <4082369+lathoub@users.noreply.github.com>
2021-03-28 09:35:51 +0200

    remove booting msg

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ require('dotenv').config();
 var env = process.env.NODE_ENV || 'development';
 var config = require('./config')[env];
 
-debug('booting..');
-
 app.listen(config.express.port, function (error) {
   if (error) {
     debug('Unable to listen for connections', error)
