458691b26e316299cdf65fef971e60cbd353dfe9
lathoub <4082369+lathoub@users.noreply.github.com>
2024-08-24 16:23:20 +0200

    clean

--- route.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router()
-const asyncHandler = require('express-async-handler') // https://www.npmjs.com/package/express-async-handler https://zellwk.com/blog/async-await-express/
-const registrar = require('./controllers/registrar')
-
-router.post('/', asyncHandler(registrar.Register))
-router.get('/Things(:id)', asyncHandler(registrar.Things))
-
-module.exports = router
